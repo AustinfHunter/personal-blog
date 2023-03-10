1918393120f277fc57d14bc928cddadad22d3191
austinfhunter <austinfhunter@gmail.com>
2023-03-10 00:23:29 -0500

    Added support for scrolling to elements by ID using url hash in Post

--- client/src/pages/Post.jsx
+++ client/src/pages/Post.jsx
@@ -7,41 +7,53 @@ import remarkGfm from 'remark-gfm'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { coldarkDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
-function Post(){
+function Post() {
     const url = process.env.REACT_APP_API_URL + "posts/" + useParams().post
-    console.log("URL: " + url )
+    console.log("URL: " + url)
     const [post, setPost] = useState({})
 
-    useEffect(()=>{
+    useEffect(() => {
+        window.addEventListener("hashchange", handleHash)
         fetch(url).then(res => res.json())
-        .then(data => {
-            setPost(data.post)
-            document.title = data.post.title
-        })
+            .then(data => {
+                setPost(data.post)
+                document.title = data.post.title
+            })
+        return () => { window.removeEventListener("hashchange", handleHash) }
     }, [])
 
-    return(
+    const handleHash = () => {
+        const hash = window.location.hash
+        if (hash.length) {
+            const element = document.getElementById(hash.substring(1))
+            if (element) {
+                element.scrollIntoView()
+            }
+        }
+    }
+
+    return (
         <div className="post">
             <div className="content">
-            <h1>{post.title}</h1>
-            <ReactMarkdown children={post.content} remarkPlugins={[remarkGfm]} className="markdown" components={{
-                code({ node, inline, className, children, ...props }) {
-                    const match = /language-(\w+)/.exec(className || '')
-                    return !inline && match ? (
-                        <SyntaxHighlighter
-                            children={String(children).replace(/\n$/, '')}
-                            style={coldarkDark}
-                            language={match[1]}
-                            PreTag="div"
-                            {...props}
-                        />
-                    ) : (
-                        <code className={className} {...props}>
-                            {children}
-                        </code>
-                    )
-                }
-            }} />
+                <h1>{post.title}</h1>
+                <ReactMarkdown children={post.content} remarkPlugins={[remarkGfm]} className="markdown" components={{
+                    code({ node, inline, className, children, ...props }) {
+                        const match = /language-(\w+)/.exec(className || '')
+                        return !inline && match ? (
+                            <SyntaxHighlighter
+                                children={String(children).replace(/\n$/, '')}
+                                style={coldarkDark}
+                                language={match[1]}
+                                PreTag="div"
+                                {...props}
+                            />
+                        ) : (
+                            <code className={className} {...props}>
+                                {children}
+                            </code>
+                        )
+                    }
+                }} />
             </div>
         </div>
     )
